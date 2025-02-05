598c4c444e5f379cac79b70d3aaa1711f271f5d0
gustavofmacia <gustavofmacia@gmail.com>
2025-02-05 18:26:55 -0300

    feat: add custom Tailwind configuration file

--- tailwind-custom.config.ts
+++ tailwind-custom.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const tailwindCustomConfig: Config = {
+export default {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,16 +9,22 @@ const tailwindCustomConfig: Config = {
     container: {
       center: true,
       padding: {
-        DEFAULT: "1rem",
-        xs: "4.8rem",
-        sm: "4.8rem",
-        md: "0rem",
-        lg: "0rem",
-        xl: "0rem",
+        "DEFAULT": "1rem",
+        "xs": "4.8rem",
+        "sm": "4.8rem",
+        "md": "0rem",
+        "lg": "0rem",
+        "xl": "0rem",
         "2xl": "0rem",
       },
     },
+    extend: {
+      fontSize: {
+        "1.2xl": ["1.375rem", "1.625rem"],
+        "2.6xl": ["1.625rem", "2.125rem"],
+        "2.8xl": ["1.75rem", "2.125rem"],
+        "4.2xl": ["2.5rem", "2.8rem"],
+      },
+    },
   },
-};
-
-export default tailwindCustomConfig;
+} satisfies Config;
