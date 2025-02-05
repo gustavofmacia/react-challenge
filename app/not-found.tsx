99a1e8a8397458b5b31303f2fd19c94b05a167b1
gustavofmacia <gustavofmacia@gmail.com>
2025-02-05 18:34:12 -0300

    feat: create NotFound component in root

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/shadcn/button";
+import { MdHome } from "react-icons/md";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <section className="flex min-h-[50vh] flex-col items-center justify-center gap-2">
+      <h1 className="mb-2 text-center text-xl font-bold sm:text-2xl">
+        Page not found
+      </h1>
+      <Button
+        variant="link"
+        className="flex h-auto p-0 text-lg sm:text-xl"
+        asChild
+      >
+        <Link href="/" prefetch={false}>
+          <MdHome className="!size-5 sm:!size-6" />
+          Go to home page
+        </Link>
+      </Button>
+    </section>
+  );
+}
