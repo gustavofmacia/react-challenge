c821b380388fb1199bf031d7c8344e1991d4986a
gustavofmacia <gustavofmacia@gmail.com>
2025-02-05 19:09:58 -0300

    feat: update RootLayout with Suspense for loading ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,22 +4,42 @@ import { roboto } from "./fonts";
 import "./globals.css";
 // Components
 import Header from "@/ui/layout/header";
+// React
+import { Suspense } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export const metadata: Metadata = {
-  title: "Cloud Music",
-  description: "Cloud Music is a music streaming service.",
+  title: "Music Admin",
+  description:
+    "Manage and organize your music collection in the cloud with Music Admin.",
 };
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${roboto.className} antialiased`}>
         <Header />
-        <main>{children}</main>
+        <Suspense
+          fallback={
+            <>
+              <div className="flex h-dvh w-full items-center justify-center pb-72">
+                <AiOutlineLoading3Quarters className="size-28 animate-spin text-primary" />
+              </div>
+              <span className="sr-only">Loading web site</span>
+            </>
+          }
+        >
+          <main>
+            {modal}
+            {children}
+          </main>
+        </Suspense>
       </body>
     </html>
   );
