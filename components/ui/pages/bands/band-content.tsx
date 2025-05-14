93c6c53f91d30588019020e8f02a5253cf9e89a5
gustavofmacia <gustavofmacia@gmail.com>
2025-05-14 10:53:32 -0300

    fix: add validations

--- components/ui/pages/bands/band-content.tsx
+++ components/ui/pages/bands/band-content.tsx
@@ -1,37 +1,43 @@
 type Props = { band: Band };
 
 export default function BandContent({ band }: Props) {
+  const { genreCode, country, year, members, albums } = band;
+
   return (
     <div className="flex flex-col gap-4">
       <div className="space-y-0.5">
         <h3>
-          <span className="font-medium">Genre:</span> {band.genreCode}
+          <span className="font-medium">Genre:</span> {genreCode}
         </h3>
         <h3>
-          <span className="font-medium">Country:</span> {band.country}
+          <span className="font-medium">Country:</span> {country}
         </h3>
         <h3>
-          <span className="font-medium">Year:</span> {band.year}
+          <span className="font-medium">Year:</span> {year}
         </h3>
       </div>
 
-      <div>
-        <h3 className="font-medium">Members</h3>
-        <ul className="grid list-disc grid-cols-2 gap-x-5 pl-5">
-          {band.members.map((member) => (
-            <li key={member.name}>{member.name}</li>
-          ))}
-        </ul>
-      </div>
+      {members?.length > 0 && (
+        <div>
+          <h3 className="font-medium">Members</h3>
+          <ul className="grid list-disc grid-cols-2 gap-x-5 pl-5">
+            {members.map((member) => (
+              <li key={member.name}>{member.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
 
-      <div>
-        <h3 className="font-medium">Albums</h3>
-        <ul className="grid list-disc grid-cols-2 gap-x-5 pl-5">
-          {band.albums.map((album) => (
-            <li key={album.name}>{album.name}</li>
-          ))}
-        </ul>
-      </div>
+      {albums?.length > 0 && (
+        <div>
+          <h3 className="font-medium">Albums</h3>
+          <ul className="grid list-disc grid-cols-2 gap-x-5 pl-5">
+            {albums.map((album) => (
+              <li key={album.name}>{album.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
